70c96c49188e0e07ff3d3bbe84aaefb8cb71f036
Paul Grigoras <paul.grigoras90@gmail.com>
2015-03-22 08:08:50 +0000

    A simple example of sending a command from the console to cubemu.

--- public/console.js
+++ public/console.js
@@ -2,12 +2,25 @@ window.onload = function () {
   setInterval(updateTime, 1000);
 }
 
+function sendCommand() {
+  console.log('Sending request');
+  var req = new XMLHttpRequest();
+  // XXX change request based on command type
+  req.open('POST', '/commands/tmpread', false);
+  req.send();
+  return req.responseText;
+}
+
 function handle_input(e){
 	if(e.keyCode === 13){
 	    var console_output = document.getElementById("console_output");
 	    var console_input = document.getElementById("console_input");
 	    console_output.innerHTML += "<div>"+console_input.value+"</div>";
 	    console_input.value = "";
+      // XXX parse console input to find command type and params
+      // for now just assume the command is a temp read
+      var response = sendCommand();
+      console_output.innerHTML += "<div> >"+response + "</div>";
 	}
 }
 
--- server.js
+++ server.js
@@ -14,7 +14,32 @@ app.get('/console', function(req, res) {
   res.sendFile(__dirname + '/public/input.html');
 });
 
+var net = require('net');
+// Route for commands
+app.post('/commands/tmpread', function(req, res) {
+  console.log('  Received command TMP_READ');
+  var client = net.connect(
+                {port: 3000},
+                function() { //'connect' listener
+                  console.log('  --> Connected to FakeSat!');
+                  client.write('TMP_READ');
+                });
+
+  client.on('data', function(data) {
+    console.log('  --> FakeSat response: ' + data.toString());
+    res.send(data);
+    client.end();
+  });
+
+  client.on('end', function() {
+    console.log('  --> Closing connection to FakeSat!');
+  });
+});
+
 app.use(express["static"]('public'));
 
-http.createServer(app).listen(config.port);
+var server = http.createServer(app).listen(config.port);
 console.log("Server running on port " + config.port);
+
+// TODO add command for requesting latest image
+// TODO add command for requesting current position
